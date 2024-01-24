da2fd7170c1fe1ced9dc67f7ce4a7eb79a68b672
Blackbird65536 <149343936+Blackbird65536@users.noreply.github.com>
2024-01-24 13:06:20 -0500

    Update keepyourheadupkids.py

--- keepyourheadupkids.py
+++ keepyourheadupkids.py
@@ -1,15 +1,14 @@
 import math
 import random
-
-print(random.randint(1, 10**6))
+import numpy as np
 
 nums1 = []
 nums2 = []
 
-for i in range(35000):
+for i in range(6000):
   skill = random.randint(1,10**9)
   luck = random.randint(1,10**9)
-  nums1.append(0.99*skill + 0.01*luck)
+  nums1.append(0.95*skill + 0.05*luck)
   nums2.append(skill)
 
 def maximum(n):
@@ -23,8 +22,6 @@ def maximum(n):
       continue
   return orderedpair
   
-print(maximum([1,2,3,4,5,6]))
-  
 
 def multimaximum(m,n):
   maximumset = []
@@ -33,10 +30,26 @@ def multimaximum(m,n):
     maximumset.append(relativemaximum)
     n[relativemaximum[1]] = -1
   return maximumset
+
+
+pureskill = multimaximum(300, nums2)
+tinyluck = multimaximum(300,nums1)
+pureskillindex = []
+tinyluckindex = []
+i = 0
+for i in range(len(pureskill)):
+  pureskillindex.append(pureskill[i][1])
+  tinyluckindex.append(tinyluck[i][1])
   
-print(multimaximum(2, [1,2,16,4,5,3]))
-print(multimaximum(12, nums1))
-print(multimaximum(12, nums2))
+print(pureskillindex)
+print(tinyluckindex)
+
+        
+
+concatenated = np.concatenate((pureskillindex, tinyluckindex))
+
+print(600 - len(list(set(concatenated))))
+
 
 
 
